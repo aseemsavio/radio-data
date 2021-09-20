7e706fbb8a44c9c2389d8ad25dc9a27d7676cdf7
aseemsavio <aseemsavio3@gmail.com>
2021-09-20 10:59:32 +0530

    streaming url

--- src/main/kotlin/indian/IndianRadioScraper.kt
+++ src/main/kotlin/indian/IndianRadioScraper.kt
@@ -24,7 +24,39 @@ private class IndianRadioScraper(
 
             content.forEach {
                 val items = it.select("a")
-                items.forEach { element -> println("Item: $element") }
+                items.map { element ->
+                    /*CoroutineScope(Dispatchers.Default).async {*/
+                    val station = Station()
+                    station.name = element.select("p").first()?.text().toString()
+                    val productUrl = element.absUrl("href")
+                    println(productUrl)
+                    val productContent = connect(productUrl).getElementsByClass("content")
+                    val titleElement = productContent.first()?.getElementById("radio-pagetitle")
+                    station.name = titleElement?.text() ?: ""
+                    val image = productContent
+                        .first()
+                        ?.getElementsByClass("logotip_new")
+                        ?.select("img")
+                        ?.first()
+                        ?.absUrl("src")
+                    station.imageUrl = image ?: ""
+
+                    //val audioUrl = productContent.first()?.getElementById("player")
+                    val script = productContent.select("script").first().toString()
+                    //println("script: $script")
+                    val audioUrl = script?.let { item ->
+                        if (item.isNotEmpty() && item.contains("file:\"") && item.contains("\", autoplay")) getBetweenStrings(
+                            item,
+                            "file:\"",
+                            "\", autoplay"
+                        ) else ""
+                    }
+                    station.streamingUrl = audioUrl ?: ""
+                    println("Data: $station")
+                    // enter into each product's page
+
+                }
+                /*}.awaitAll()*/
             }
 
             //println("$content \n\n")
@@ -39,6 +71,28 @@ private class IndianRadioScraper(
 
     private suspend fun connect(url: String): Document =
         withContext(Dispatchers.IO) { SSLHelper.getConnection(url).get() }
+
+    private fun getBetweenStrings(
+        text: String,
+        textFrom: String,
+        textTo: String?
+    ): String? {
+        var result = ""
+
+        // Cut the beginning of the text to not occasionally meet a
+        // 'textTo' value in it:
+        result = text.substring(
+            text.indexOf(textFrom) + textFrom.length,
+            text.length
+        )
+
+        // Cut the excessive ending of the text:
+        result = result.substring(
+            0,
+            result.indexOf(textTo!!)
+        )
+        return result
+    }
 }
 
 suspend fun scrapeIndianRadio() = IndianRadioScraper(MAIN_DIRECTORY, "https://onlineradiofm.in").scrape()
--- src/main/kotlin/indian/RadioStation.kt
+++ src/main/kotlin/indian/RadioStation.kt
@@ -12,12 +12,53 @@ data class Station(
     val languages: List<String>,
     val genre: List<String>,
     val firstAiredYear: String?,
-    val bitRate: String,
-    val frequency: String,
-    val location: String?
+    val bitRate: String?,
+    val frequency: String?,
+    val location: List<String>?,
+    val rating: Rating?
+)
+
+@Serializable
+data class Rating(
+    val ratingOnFive: Float,
+    val numberOfVotes: Int
 )
 
 @Serializable
 data class Stations(
-    val stations: List<Station>
+    val stations: Sequence<Station>
 )
+
+data class StationBuilder(
+    var name: String = "",
+    var tagLine: String = "",
+    var description: String = "",
+    var imageUrl: String = "",
+    var streamingUrl: String = "",
+    var languages: List<String> = listOf(),
+    var genre: List<String> = listOf(),
+    var firstAiredYear: String? = null,
+    var bitRate: String? = null,
+    var frequency: String? = null,
+    var location: List<String> = listOf(),
+    var rating: Rating? = null
+) {
+    fun build(): Station {
+        return Station(
+            name,
+            tagLine,
+            description,
+            imageUrl,
+            streamingUrl,
+            languages,
+            genre,
+            firstAiredYear,
+            bitRate,
+            frequency,
+            location,
+            rating
+        )
+    }
+}
+
+fun Station() = StationBuilder()
